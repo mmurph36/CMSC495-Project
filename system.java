90be6fdd0228c3ee468f20b3cba292b2b728b8e6
klotfian <klotfian@gmail.com>
2019-02-13 18:30:40 -0500

    Add files via upload

--- system.java
+++ system.java
@@ -13,8 +13,18 @@ public class system {
 		d_list = new ArrayList<doctor>();
 		
 		//sample patient object within the patient list
-		p_list.add(new patient("a", "a","a","a","a","a",5,6,5555, "a", "a"));
+		p_list.add(new patient("Amuro", "Ray","N/A","whitedevil","password","11/4/0063",29,7979,5555, "address", "205-345-3452"));
+		p_list.add(new patient("Char", "Aznable","N/A","redcomet","password","11/17/0059",33,5959,7777, "address", "205-346-3562"));
+		p_list.add(new patient("Haman", "Karn","N/A","qubeley","password","1/10/0081",22,8989,8888, "address", "235-645-3294"));
+		p_list.add(new patient("Judau", "Ashta","N/A","zz","password","9/10/0073",15,1432,9999, "address", "205-345-3452"));
+		p_list.add(new patient("Kamille", "Bidan","N/A","zeta","password","11/11/0069",18,9376,6666, "address", "205-345-3452"));
 		
+		//sample doctor object within the doctor list
+		d_list.add(new doctor("Joseph", "Joestar", "N/A", "JJ", "password", 1111, 1, 80));
+		d_list.add(new doctor("Jotaro", "Kujo", "N/A", "JK", "password", 2222, 1, 30));
+		d_list.add(new doctor("Josuke", "Higashikata", "N/A", "JH", "password", 3333, 1, 16));
+		d_list.add(new doctor("Giorno", "Giovanna", "N/A", "GG", "password", 4444, 1, 15));
+		d_list.add(new doctor("Jolyne", "Cujoh", "N/A", "JC", "password", 5555, 1, 19));
 	}
 	
 	//will return false if patient already exists within database
@@ -27,6 +37,15 @@ public class system {
 		p_list.add(new patient(f_name, l_name, m_name, user_name, password, dob, age, SSN, zip, address, p_number));
 		return true;
 	}
+	
+	//official way to check if the doctor exists
+		public boolean doctor_exists(int id) {
+			for(doctor d : d_list) {
+				if(d.id == id) return true;
+			}
+			
+			return false;
+		}
 	
 	//official way to check if the patient exists
 	public boolean patient_exists(int SSN) {
