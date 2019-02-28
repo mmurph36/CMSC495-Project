4436fc808bac1466c8918eff5beb113c0e7cd06f
klotfian <klotfian@gmail.com>
2019-02-27 21:00:44 -0500

    Update patient.java

--- patient.java
+++ patient.java
@@ -5,9 +5,9 @@
 public class patient extends person{
     int SSN, zip;
     String address, p_number, city, state;
-
+    boolean policy;
     public patient(String f_name, String l_name, String m_name, String user_name, String password, String dob,
-                   int SSN, int zip, String address, String city, String state, String p_number) {
+                   int SSN, int zip, String address, String city, String state, String p_number, boolean policy) {
 
         super(f_name, l_name, m_name, user_name, password, dob);
 
@@ -17,6 +17,7 @@ public class patient extends person{
         this.p_number = p_number;
         this.city = city;
         this.state = state;
+        this.policy = policy;
     }
 
     public String toString() {
