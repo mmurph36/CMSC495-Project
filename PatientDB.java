a4a535a5c763032ca488cd3b2f7d8b59d6740827
mmurph36 <46504033+mmurph36@users.noreply.github.com>
2019-03-02 14:25:49 -0600

    Add files via upload

---
+++ PatientDB.java
@@ -0,0 +1,66 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class PatientDB {
+
+    /**
+     * Connect to the test.db database
+     * @return the Connection object
+     */
+
+    String sql = "SELECT l_name, f_name, m_name, user_name, password, dob, SSN, zip, address, city, state, p_number, policy" +
+            "       FROM patientinfo";
+
+    ArrayList<patient> pat_list;
+
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/patients.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+
+    /**
+     * select all rows in the table
+     */
+    public ArrayList<patient> selectAll(){
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            pat_list = new ArrayList<patient>();
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getString("l_name") +  "\t" +
+                        rs.getString("f_name") + "\t" +
+                        rs.getString("m_name"));
+
+                pat_list.add(new patient(rs.getString("f_name"), rs.getString("l_name"),
+                            rs.getString("m_name"), rs.getString("user_name"),
+                            rs.getString("password"), rs.getString("dob"),
+                            rs.getInt("SSN"),rs.getInt("zip"),rs.getString("address"),
+                            rs.getString("city"),rs.getString("state"),rs.getString("p_number"),
+                            rs.getBoolean("policy")));
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return pat_list;
+    }
+}
