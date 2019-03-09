337ccc14f476a561d2d6c28b21b4ca9562b5ef88
mmurph36 <46504033+mmurph36@users.noreply.github.com>
2019-03-08 18:13:09 -0600

    Update staff.java

--- staff.java
+++ staff.java
@@ -1,22 +1,22 @@
 /**
  * @author Kamran Lotfian
- * basic doctor class with its own unique toString method
+ * basic staff class with its own unique toString method
  */
 public class staff extends person {
-    int id, access_level;
+    private int id, access_level;
 
     public staff(String f_name, String l_name, String m_name, String user_name, String password, int id, int access_level) {
         super(f_name, l_name, m_name, user_name, password, "1/1/1111");
 
         this.id = id;
-        this.access_level = access_level;
     }
 
+    public int getId() { return id; }
+
     public String toString() {
-        String s = "*****Doctor Details*****\n";
+        String s = "*****Staff Details*****\n";
         s += super.toString();
         s += "\nID: " + id;
-        s += "\nAccess Level: " + access_level;
 
         return s;
     }
